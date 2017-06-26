a619274521cefebcdc94a9a6f9c4b71764a4b37e
Quentin Armitage <quentin@armitage.org.uk>
2017-06-26 23:24:03 +0100

    more travis testing

--- test/ci/setup.sh
+++ test/ci/setup.sh
@@ -13,3 +13,12 @@ cat /etc/*release
 sudo apt list --installed
 
 sudo apt -qq install libsnmp-dev iptables-dev libipset-dev libnfnetlink-dev libnl-3-dev libnl-genl-3-dev libnl-route-3-dev libssl-dev
+
+echo; echo /usr/include/linux/ip_vs.h; echo ===================
+cat /usr/include/linux/ip_vs.h
+
+echo; echo /usr/include/linux/if.h; echo ===================
+cat /usr/include/linux/if.h
+
+echo; echo /usr/include/net/if.h; echo ===================
+cat /usr/include/net/if.h
