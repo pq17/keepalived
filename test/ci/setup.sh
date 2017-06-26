b14d01e0bd21cbda5152f36160fcbedf62cdbd43
Quentin Armitage <quentin@armitage.org.uk>
2017-06-26 22:59:34 +0100

    Add additional -dev packages in travis build

--- test/ci/setup.sh
+++ test/ci/setup.sh
@@ -12,4 +12,4 @@ cat /etc/*release
 # Packages installed
 sudo apt list --installed
 
-sudo apt -qq install libsnmp-dev
+sudo apt -qq install libsnmp-dev iptables-dev libipset-dev libnfnetlink-dev libnl-3-dev libnl-genl-3-dev libnl-route-3-dev libssl-dev
