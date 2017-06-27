9190d367ceb1d8142cb288dea3669c51974e0777
Quentin Armitage <quentin@armitage.org.uk>
2017-06-27 14:01:28 +0100

    Another attempt to get up-to-date kernel headers in travis

--- test/ci/setup.sh
+++ test/ci/setup.sh
@@ -16,7 +16,15 @@
 sudo apt -qq install libsnmp-dev iptables-dev libipset-dev libnfnetlink-dev libnl-3-dev libnl-genl-3-dev libnl-route-3-dev libssl-dev
 
 # Update kernel headers to match kernel
-pushd /tmp
-wget http://security.ubuntu.com/ubuntu/pool/main/l/linux/linux-libc-dev_4.4.0-81.104_amd64.deb
-sudo dpkg --install linux-libc-dev_4.4.0-81.104_amd64.deb
-popd
+type pushd
+type popd
+
+# pushd /tmp
+CURDIR=$(pwd)
+cd /tmp
+KER_VER=$(apt list --installed | grep "^linux-image-[0-9]" | sed -e "s/.*,now //" -e "s/~.*//)
+wget http://security.ubuntu.com/ubuntu/pool/main/l/linux/linux-libc-dev_$(KER_VER)_amd64.deb
+sudo dpkg --install linux-libc-dev_$(KER_VER)_amd64.deb
+rm linux-libc-dev_$(KER_VER)_amd64.deb
+# popd
+cd $CURDIR
